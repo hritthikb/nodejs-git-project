33a76d58549548bcd7d635c4495ba2ce718973c2
hritthikb <hritthikbose@gmail.com>
2021-05-14 21:24:15 +0530

    This is the first commit Server has been created

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+http
+    .createServer((req, res) => {
+        console.log(res);
+        res.write("<h1> Home Page !! </h1>");
+        res.end();
+    })
+    .listen(8080);
+
